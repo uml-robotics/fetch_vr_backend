faef82137b664ec138881a3ea0695a625939015a
Gregory LeMasurier <gregorylemasurier@gmail.com>
2021-01-15 23:18:15 -0500

    Cleaning up initial commit

--- src/waypoint_follower.cpp
+++ src/waypoint_follower.cpp
@@ -17,11 +17,9 @@ tf::TransformListener* listener_;
 ros::Publisher plan_pub;
 ros::Publisher waypoint_error_pub;
 ros::Publisher is_done_pub;
-//ros::Publisher lastPointPub;
 moveit::planning_interface::MoveGroupInterface::Plan plan;
 std::list<moveit::planning_interface::MoveGroupInterface::Plan> planList;
-//geometry_msgs::Pose lastPose;
-//moveit::planning_interface::MoveGroupInterface::Plan lastPlan;
+
 void jointCommandsCb(const geometry_msgs::PoseArray::ConstPtr& msg)
 {
     std::vector<geometry_msgs::Pose> poses;
@@ -46,16 +44,11 @@ void jointCommandsCb(const geometry_msgs::PoseArray::ConstPtr& msg)
        moveit_msgs::RobotTrajectory trajectory;
 
         if(!planList.empty()){
-            ROS_INFO_STREAM("SETTING NEW START STATE!!!");
             robot_state::RobotState start_state(*(move_fetch_ptr->getCurrentState()));
 
-            ROS_INFO_STREAM("SETTING NEW STATE!!!");
             robot_state::RobotState state(start_state);
-            ROS_INFO_STREAM("SETTING joints!!!");
             const std::vector<double> joints = planList.back().trajectory_.joint_trajectory.points.back().positions;
-            ROS_INFO_STREAM("SETTING NEW Positions!!!");
             state.setJointGroupPositions(move_fetch_ptr->getName(), joints);
-            ROS_INFO_STREAM("SETTING NEW MG Start STATE!!!");
             move_fetch_ptr->setStartState(state);
         }
 
@@ -77,8 +70,6 @@ void jointCommandsCb(const geometry_msgs::PoseArray::ConstPtr& msg)
         plan_pub.publish(plan.trajectory_.joint_trajectory);
         currentIndex++;
     }
-    //lastPose = poses.back();
-    //lastPlan = planList.back();
 }
 
 void onNewTrajectory(const std_msgs::Bool::ConstPtr& msg){
@@ -89,26 +80,6 @@ void onNewTrajectory(const std_msgs::Bool::ConstPtr& msg){
     }
 }
 
-/*
-void confirmationCb(const std_msgs::Bool::ConstPtr& msg)
-{
-    if(msg->data){
-        std_msgs::Bool msg;
-        msg.data = false;
-        is_done_pub.publish(msg);
-        ROS_INFO_STREAM("Publishing Trajectories!");
-        std::list<moveit::planning_interface::MoveGroupInterface::Plan>::iterator iter;
-        for(iter = planList.begin(); iter != planList.end(); iter++)
-        {
-            ROS_INFO_STREAM("Publishing Trajectories.......!");
-            plan_pub.publish(iter->trajectory_.joint_trajectory);
-        }
-        ROS_INFO_STREAM("Done Publishing Trajectories!");
-        msg.data = true;
-        is_done_pub.publish(msg);
-    }
-}
-*/
 void confirmationCB(const std_msgs::Bool::ConstPtr& msg)
 {
     if(msg->data){
@@ -131,10 +102,8 @@ int main(int argc, char** argv){
 
     ros::Subscriber goal_sub = nh.subscribe("/gripper_goal", 1000, jointCommandsCb);
     ros::Subscriber confirmation_sub = nh.subscribe("/joint_plan_confirmation", 1000, confirmationCB);
-    //ros::Subscriber execute_next_sub = nh.subscribe("/joint_plan_execute_next", 1000, onExecuteNext);
     ros::Subscriber trajectory_signal_sub = nh.subscribe("/joint_plan_signal", 1000, onNewTrajectory);
     plan_pub = nh.advertise<trajectory_msgs::JointTrajectory>("/joint_plan", 1000);
-    //lastPointPub = nh.advertise<geometry_msgs::Pose>("/test/last_point", 1000);
     is_done_pub = nh.advertise<std_msgs::Bool>("/is_joint_plan_sent", 1000);
     waypoint_error_pub = nh.advertise<std_msgs::UInt8>("/out_of_range_waypoints", 1000);
 
