0abd754d74cc97a854bb1a6fd769247e26706d72
Gregory LeMasurier <gregorylemasurier@gmail.com>
2021-01-18 23:16:32 -0500

    fixed nav node to work with vr publisher

--- src/nav.cpp
+++ src/nav.cpp
@@ -1,21 +1,20 @@
 #include <ros/ros.h>
+#include <geometry_msgs/PoseArray.h>
 #include <geometry_msgs/PoseStamped.h>
-#include <nav_msgs/Path.h>
 #include <actionlib/client/simple_action_client.h>
 #include <move_base_msgs/MoveBaseGoal.h>
 #include <move_base_msgs/MoveBaseAction.h>
 
 actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction>* client_ptr;
 
-void navGoalCb(const nav_msgs::Path::ConstPtr& msg)
+void navGoalCb(const geometry_msgs::PoseArray::ConstPtr& msg)
 {
-    int i = 0;
-    int n = sizeof(msg->poses)/sizeof(msg->poses[0]);
-    ROS_INFO_STREAM("POSES: " + n);
-    for(i = 0; i < n; i++)
+    for(geometry_msgs::Pose p : msg->poses)
     {
+        ROS_INFO_STREAM("NEW NAV GOAL!");
         move_base_msgs::MoveBaseGoal goal;
-        goal.target_pose = msg->poses[i];
+        goal.target_pose.pose = p;
+	goal.target_pose.header.frame_id = "map";
         client_ptr->sendGoal(goal);
     }
 }
@@ -26,6 +25,7 @@ int main(int argc, char** argv){
 
     actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> client("move_base");
     client_ptr = &client;
+    ROS_INFO_STREAM("STARTING NAV NODE!");
 
     ros::Subscriber goal_sub = nh.subscribe("/navigation_goal", 1000, navGoalCb);
 
