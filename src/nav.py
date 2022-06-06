e84e92ba0d0a74b1c2d74b641e53a55b99f0e134
jacobe90 <jacobepstein90@gmail.com>
2022-06-06 10:43:22 -0700

    added spot nav backend support

--- src/nav.py
+++ src/nav.py
@@ -1,22 +1,58 @@
+#! /usr/bin/env python3
 import rospy
-from std_msgs.msg import Boolean
+from std_msgs.msg import Bool
 from geometry_msgs.msg import PoseArray
-poses = []
+from spot_msgs.msg import TrajectoryAction, TrajectoryGoal
+import tf2_ros
+import actionlib
 
-def nav_goal_cb(msg):
-    # unpack the pose array, add each pose to the array poses
-    pass
 
-def confirmation_cb(msg):
-    if msg.data:
-        # pop front pose off of poses
-        # send pose to the spot-ros handle trajectory server
-        pass
+class Nav:
+    def __init__(self):
+        self.poses = []
+        self.result_pub = rospy.Publisher("/spot_nav_result", Bool, queue_size=10)
+        rospy.Subscriber("/nav_confirmation", Bool, self.confirmation_cb)
+        rospy.Subscriber("/navigation_goal", PoseArray, self.nav_goal_cb)
 
-def main():
-    rospy.init_node('fetch-nav-backend')
-    rospy.Subscriber("/nav_confirmation", Boolean, confirmation_cb)
-    rospy.Subscriber("/navigation_goal", PoseArray, nav_goal_cb)
+    def nav_goal_cb(self, msg):
+        # unpack the pose array, add each pose to the array poses
+        self.poses = msg.poses
+
+    def confirmation_cb(self, msg):
+        if msg.data:
+            # pop front pose off of poses
+            if len(self.poses) <= 0:
+                rospy.logerr("[SPOT_NAV]: Confirmation sent but no goal poses currently queued")
+                return
+            # send pose to the spot-ros handle trajectory server
+            # Creating action client
+            client = actionlib.SimpleActionClient('/spot/trajectory', TrajectoryAction)
+
+            # Waiting for action server
+            client.wait_for_server()
+
+            # Creating and setting parameters of the action goal
+            trajectory_goal = TrajectoryGoal()
+            pose = self.poses.pop(0)
+            pose.frame_id = "vision"
+            tf_buffer = tf2_ros.Buffer()
+            trajectory_goal.target_pose = tf_buffer.transform(pose, "body", rospy.Duration(1))
+            trajectory_goal.target_pose = pose
+            trajectory_goal.precise_positioning = True
+            trajectory_goal.duration.data = rospy.Duration(2)
+
+            # send goal to the action server, wait for result, print result to console
+            client.send_goal(trajectory_goal)
+            client.wait_for_result()
+            result = client.get_result()
+            rospy.loginfo(result)
+
+            # Send result to unity
+            self.result_pub.publish(result.success)
+
 
 if __name__ == "__main__":
-    main()
+    rospy.init_node('fetch_backend')
+    spotNav = Nav()
+    rospy.loginfo("[SPOT_NAV]: Backend up and running!")
+    rospy.spin()
