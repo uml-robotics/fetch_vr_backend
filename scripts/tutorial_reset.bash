4ec0a011ed346730251e5683d7fe85fab6a95851
GregoryLeMasurier <gregorylemasurier@gmail.com>
2022-08-09 16:34:46 -0400

    fix typo

--- scripts/tutorial_reset.bash
+++ scripts/tutorial_reset.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "RESETTING FOR MANIPULATION..."
+echo "RESETTING FOR TUTORIAL..."
 bash $(rospack find fetch_vr_backend)/scripts/gripper_reset.bash
 bash $(rospack find fetch_vr_backend)/scripts/head_reset.bash
 bash $(rospack find fetch_vr_backend)/scripts/torso_up.bash
