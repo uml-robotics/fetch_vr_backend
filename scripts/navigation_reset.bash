e96031d6c05a3518b2dd46d1641bce47bb3acd08
Gregory LeMasurier <gregorylemasurier@gmail.com>
2022-08-22 15:47:32 -0400

    update localization in reset scripts

--- scripts/navigation_reset.bash
+++ scripts/navigation_reset.bash
@@ -19,51 +19,51 @@ echo "\n\n\nRobot is reset.\nPlease move the robot to the starting square. Then 
 select val in "1" "2" "3" "Exit"; do
   case $val in
     1 )
-      rostopic pub -1 /initialpose geometry_msgs/PoseWithCovarianceStamped "header:
-          seq: 0
-          stamp:
-            secs: 0
-            nsecs: 0
-          frame_id: 'map'
-        pose:
-          pose:
-            position: {x: 2.74453091621, y: 0.512610197067, z: 0.0}
-            orientation: {x: 0.0, y: 0.0, z: -0.999975106018, w: 0.00705601479506}
-          covariance: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0]"
+rostopic pub -1 /initialpose geometry_msgs/PoseWithCovarianceStamped "header:
+  seq: 0
+  stamp:
+    secs: 0
+    nsecs: 0
+  frame_id: 'map'
+pose:
+  pose:
+    position: {x: 2.5540060997, y: 0.499838203192, z: 0.0}
+    orientation: {x: 0.0, y: 0.0, z: -0.999245881735, w: 0.0388286986114}
+  covariance: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0]"
         break
         ;;
     2 )
-      rostopic pub -1 /initialpose geometry_msgs/PoseWithCovarianceStamped "header:
-          seq: 0
-          stamp:
-            secs: 0
-            nsecs: 0
-          frame_id: 'map'
-        pose:
-          pose:
-            position: {x: 1.67415261269, y: 2.00741744041, z: 0.0}
-            orientation: {x: 0.0, y: 0.0, z: -0.999975106018, w: 0.00705601479506}
-          covariance: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0]"
+rostopic pub -1 /initialpose geometry_msgs/PoseWithCovarianceStamped "header:
+  seq: 0
+  stamp:
+    secs: 0
+    nsecs: 0
+  frame_id: 'map'
+pose:
+  pose:
+    position: {x: 1.67415261269, y: 2.00741744041, z: 0.0}
+    orientation: {x: 0.0, y: 0.0, z: -0.999975106018, w: 0.00705601479506}
+  covariance: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0]"
       break
       ;;
     3 )
-      rostopic pub -1 /initialpose geometry_msgs/PoseWithCovarianceStamped "header:
-          seq: 0
-          stamp:
-            secs: 0
-            nsecs: 0
-          frame_id: 'map'
-        pose:
-          pose:
-            position: {x: 1.64061737061, y: 0.722720503807, z: 0.0}
-            orientation: {x: 0.0, y: 0.0, z: 0.700711730177, w: 0.713444511642}
-          covariance: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0]"
+rostopic pub -1 /initialpose geometry_msgs/PoseWithCovarianceStamped "header:
+  seq: 0
+  stamp:
+    secs: 0
+    nsecs: 0
+  frame_id: 'map'
+pose:
+  pose:
+    position: {x: 1.56371152401, y: 0.745879352093, z: 0.0}
+    orientation: {x: 0.0, y: 0.0, z: 0.69715230447, w: 0.716923053313}
+  covariance: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0]"
       break
       ;;
     Exit ) exit;;
