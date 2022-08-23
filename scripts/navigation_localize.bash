41f4afd51d3e0aee75e047468cce9f777583b185
Gregory LeMasurier <gregorylemasurier@gmail.com>
2022-08-23 15:05:59 -0400

    split localization scripts from reset

---
+++ scripts/navigation_localize.bash
@@ -0,0 +1,56 @@
+#!/bin/bash
+echo -e "\n\n\nPlease move the robot to the starting square. Then please select the run number."
+select val in "1" "2" "3" "Exit"; do
+  case $val in
+    1 )
+rostopic pub -1 /initialpose geometry_msgs/PoseWithCovarianceStamped "header:
+  seq: 0
+  stamp:
+    secs: 0
+    nsecs: 0
+  frame_id: 'map'
+pose:
+  pose:
+    position: {x: 2.5540060997, y: 0.499838203192, z: 0.0}
+    orientation: {x: 0.0, y: 0.0, z: -0.999245881735, w: 0.0388286986114}
+  covariance: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0]"
+        break
+        ;;
+    2 )
+rostopic pub -1 /initialpose geometry_msgs/PoseWithCovarianceStamped "header:
+  seq: 0
+  stamp:
+    secs: 0
+    nsecs: 0
+  frame_id: 'map'
+pose:
+  pose:
+    position: {x: 1.67415261269, y: 2.00741744041, z: 0.0}
+    orientation: {x: 0.0, y: 0.0, z: -0.999975106018, w: 0.00705601479506}
+  covariance: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0]"
+      break
+      ;;
+    3 )
+rostopic pub -1 /initialpose geometry_msgs/PoseWithCovarianceStamped "header:
+  seq: 0
+  stamp:
+    secs: 0
+    nsecs: 0
+  frame_id: 'map'
+pose:
+  pose:
+    position: {x: 1.56371152401, y: 0.745879352093, z: 0.0}
+    orientation: {x: 0.0, y: 0.0, z: 0.69715230447, w: 0.716923053313}
+  covariance: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0]"
+      break
+      ;;
+    Exit ) exit;;
+  esac
+done
+echo -e "\nRobot is localized."
--- scripts/tutorial_reset.bash
+++ scripts/tutorial_reset.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "\n\n\nResetting for tutorial.\nPlease move the robot to a safe spot to reset the arm. Then Reset."
+echo -e "\n\n\nResetting for tutorial.\nPlease move the robot to a safe spot to reset the arm. Then Reset."
 select val in "Reset" "Exit"; do
   case $val in
     Reset )
@@ -15,27 +15,8 @@ select val in "Reset" "Exit"; do
   esac
 done
 
-echo "\n\n\nRobot is reset.\nPlease move the robot to the starting square. Then Continue."
-select val in "Continue" "Exit"; do
-  case $val in
-    Continue )
-rostopic pub -1 /initialpose geometry_msgs/PoseWithCovarianceStamped "header:
-    seq: 0
-    stamp:
-      secs: 0
-      nsecs: 0
-    frame_id: 'map'
-pose:
-    pose:
-      position: {x: 1.64061737061, y: 0.722720503807, z: 0.0}
-      orientation: {x: 0.0, y: 0.0, z: -0.999992128143, w: 0.00396782707989}
-    covariance: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]"
-        break
-        ;;
-    Exit ) exit;;
-  esac
-done
-
-echo "\n\n\nRobot is ready!."
+echo -e "\nRobot is reset."
+bash $(rospack find fetch_vr_backend)/scripts/tutorial_localize.bash
+echo -e "\n\n\nRobot is ready!"
 
 
