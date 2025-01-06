f216d475d5ee55004c54657a88dff316a8eb8c54
Jordan Allspaw <allspaw.j@gmail.com>
2025-01-06 11:51:27 -0500

    added script to publish robot description as a topic

---
+++ src/republish_description.py
@@ -0,0 +1,13 @@
+import rospy
+from std_msgs.msg import String
+
+pub = rospy.Publisher('/robot_description', String, queue_size = 10)
+
+rospy.init_node('description_republisher')
+
+r = rospy.Rate(1)
+
+while not rospy.is_shutdown():
+    description = rospy.get_param("/robot_description")
+    pub.publish(description)
+    r.sleep()
